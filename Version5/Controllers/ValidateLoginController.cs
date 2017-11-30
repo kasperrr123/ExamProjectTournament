d06557ce5f03a40ebc40d11f1d137cacbbcda71d
Kasper Frank Jørgensen <kasperr_123@hotmail.com>
2017-11-30 13:07:53 +0100

    Trying to make it resize on phones

---
+++ Version5/Controllers/ValidateLoginController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Version5.Models;
+
+
+namespace Version5.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ValidateLogin")]
+    public class ValidateLoginController : Controller
+    {
+        private readonly db_examprojecttournamentContext _context;
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> PostTblLogin([FromBody] TblLogin tblLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.TblLogin.Add(tblLogin);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TblLoginExists(tblLogin.FldUsername))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTblLogin", new { id = tblLogin.FldUsername }, tblLogin);
+        }
+
+
+
+
+        private bool TblLoginExists(string id)
+        {
+            return _context.TblLogin.Any(e => e.FldUsername == id);
+        }
+
+    }
+
+  
+
+   
+
+
+}
