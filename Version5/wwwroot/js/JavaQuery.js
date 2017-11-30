bd15e73e53e455d896012f167db77f88a0c08419
daqer <david_christoffersen@live.dk>
2017-11-30 12:14:39 +0100

    GG NO should work on clients now

--- Version5/wwwroot/js/JavaQuery.js
+++ Version5/wwwroot/js/JavaQuery.js
@@ -65,20 +65,47 @@
                 }
             });
         }
+        function UploadFile() {
+            $(function () {
+                $('#projectfile').fileupload({
+                    add: function (e, data) {
+                        var jqXHR = data.submit()
+                            .success(function (result, textStatus, jqXHR) {/* ... */
+                                $('#insertPicture').val(result);
+                                alert("File Uploaded");
+                            })
+                            .error(function (jqXHR, textStatus, errorThrown) {/* ... */
+                                alert(errorThrown);
+                            });
+                    }
+                });
+            });
+        }
+        function isEmail(emailV) {
+            if (emailV != null && emailV != undefined) {
+                var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+                return pattern.test(emailV);
+            }
+            else {
+                return false;
+            }
+
+        }
 
 
         //Register Team
-        $.ajax({
-            url: "http://localhost:55655/api/tournaments",
-            type: "GET",
-            contentType: "application/json",
-            dataType: "json"
-        }).then(function (data) {
-            alert("TournamentID = " + data[data.length - 1].fldTournamentId);
-            TournamentID = data[data.length - 1].fldTournamentId;
-        }).then(function () {
-            createProject();
-        });
-
+        if (isEmail($('#email').val())) {
+            $.ajax({
+                url: "http://localhost:55655/api/tournaments",
+                type: "GET",
+                contentType: "application/json",
+                dataType: "json"
+            }).then(function (data) {
+                alert("TournamentID = " + data[data.length - 1].fldTournamentId);
+                TournamentID = data[data.length - 1].fldTournamentId;
+            }).then(function () {
+                createProject();
+            });
+        }
     });
 });
