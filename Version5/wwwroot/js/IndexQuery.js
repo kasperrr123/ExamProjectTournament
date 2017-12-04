273c5d32aa1b86c7f724beda1a4410cfb7562926
Kasper Frank Jørgensen <kasperr_123@hotmail.com>
2017-12-04 16:26:33 +0100

    Moves datetimeformat back to indexquery.

--- Version5/wwwroot/js/IndexQuery.js
+++ Version5/wwwroot/js/IndexQuery.js
@@ -33,26 +33,32 @@ $(document).ready(function getInformation() {
     $.ajax({
         url: 'http://' + hostname + '/api/Tournaments', success: function (result) {
             var address = result[result.length - 1].fldAddress;
-        $("#placeinformation").html(address)
-}
+            $("#placeinformation").html(address)
+        }
     });
-$.ajax({
-    url: 'http://' + hostname +'/api/Tournaments', success: function (result) {
-        var endTime = result[result.length - 1].fldEndDate;
-        $("#endtimeinformation").html(formatDateTime(endTime))
-    }
-});
-$.ajax({
-    url: 'http://' + hostname +'/api/Tournaments', success: function (result) {
-        var startTime = result[result.length - 1].fldStartDate;
-        $("#starttimeinformation").html(formatDateTime(startTime));
-    }
-});
+    $.ajax({
+        url: 'http://' + hostname + '/api/Tournaments', success: function (result) {
+            var endTime = result[result.length - 1].fldEndDate;
+            $("#endtimeinformation").html(formatDateTime(endTime))
+        }
+    });
+    $.ajax({
+        url: 'http://' + hostname + '/api/Tournaments', success: function (result) {
+            var startTime = result[result.length - 1].fldStartDate;
+            $("#starttimeinformation").html(formatDateTime(startTime));
+        }
+    });
 
 
 
 });
 
+function formatDateTime(datetime) {
+    var datetimeWithOutT = datetime.replace("T", " ");
+    var parts = datetimeWithOutT.split(/[- :]/);
+    var wanted = parts[2] + '/' + parts[1] + '/' + parts[0] + ' ' + parts[3] + ':' + parts[4];
+    return wanted;
+}
 
 function isCookieValid() {
     console.log(document.cookie);
