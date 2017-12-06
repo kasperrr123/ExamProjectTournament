121aae166d37ec797e0f200053ffea933dd22656
Kasper Frank Jørgensen <kasp7244@easv365.dk>
2017-12-06 21:07:21 +0100

    Created a EditQuestionnaire javascript. Created a QuestionairesController.

---
+++ Version5/wwwroot/js/EditQuestionnaire.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    getQuestions();
+
+
+
+
+});
+
+
+function getQuestions() {
+    $.ajax({
+        url: 'http://' + hostname + '/api/Questionaires',
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+    }).then(function (fromServer) {
+        $('#table').bootstrapTable({
+            data: fromServer
+        });
+    });
+};
