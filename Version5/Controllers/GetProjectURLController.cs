d389e78587d2224988ac93daa235979823d769ab
daqer <david_christoffersen@live.dk>
2017-12-06 10:50:16 +0100

    Made Changes to Controller and judgescript/page

---
+++ Version5/Controllers/GetProjectURLController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Version5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Version5.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/GetProjectURL")]
+    public class GetProjectURLController : Controller
+    {
+        private readonly db_examprojecttournamentContext _context;
+
+        public GetProjectURLController(db_examprojecttournamentContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTblTeam([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var tblTeam = await _context.TblTeam.SingleOrDefaultAsync(m => m.FldTeamName == id);
+
+            if (tblTeam == null)
+            {
+                return NotFound();
+            }
+            var tblProjectUrl = _context.TblProject.Find(tblTeam.FldProjectName).FldProjectFilePath;
+            return Ok(tblProjectUrl);
+        }
+    }
+}
