c8eddb749dda068e58c577647ec46bf00dc92f5e
Kasper Frank Jørgensen <kasperr_123@hotmail.com>
2017-12-10 19:11:36 +0100

    Started building createTournament function. I've got connection to the database and can insert fields, but i have to format the datetime from dkk to american version? i think. I've added a CSS, JAVASCRIPT and ofc a HTML document for this function.

---
+++ Version5/wwwroot/js/CreateTournament.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    var hostname = document.location.host;
+
+    $("#createTournamentBtn").click(function() {
+        postTournament();
+    });
+
+
+    function postTournament() {
+        var dataObj = {
+            fldTournamentName: $("#name").val(),
+            fldStartDate: $("#startDate").val(),
+            fldEndDate: $("#endDate").val(),
+            fldAddress: $("#address").val()
+        }
+        $.ajax({
+            url: 'http://' + hostname + '/api/tournaments',
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(dataObj),
+            success: function () {
+                alert("Success");
+            }
+        }).then(function (fromServer) {
+            alert(fromServer);
+        });
+    };
+  
+
+});
+
+
